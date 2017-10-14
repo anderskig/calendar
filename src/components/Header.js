ca4e05c25b937422585dde6d4a0f4b46fb124e30
Anders Gustafsson <andersg@gmail.com>
2017-10-14 10:30:58 +0200

    Fix so only the intended part of Month switchers are clickable, also add hover transition

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React, { PureComponent } from 'react'; 
+import PropTypes from 'prop-types';
+import Moment from 'moment';
+
 
 /* Styling */
 import './Header.css';
 
+/**
+ * Header component, displays current, next and previous months and
+ * facilitates month switching.
+*/
 class Header extends PureComponent {
 
   render() {
@@ -12,20 +19,30 @@ class Header extends PureComponent {
 
     return (
       <section className="Header">
-        <div onClick={() => onMonthChange(prevMonth)} className="prevMonth">
-          <span className="arrow">⟨</span>
-          <span className="monthName">{prevMonth.format('MMMM')}</span>
+        <div className="prevMonth">
+          <div className="changeMonthWrapper" onClick={() => onMonthChange(nextMonth)}>
+            <span className="arrow">⟨</span>
+            <span className="monthName">{prevMonth.format('MMMM')}</span>
+          </div>
         </div>
         <div className="currentMonth">
           {shownMoment.format('MMMM YYYY')}
         </div>
-        <div onClick={() => onMonthChange(nextMonth)} className="nextMonth">
-          <span className="monthName">{nextMonth.format('MMMM')}</span>
-          <span className="arrow">⟩</span>
+        <div className="nextMonth">
+          <div className="changeMonthWrapper" onClick={() => onMonthChange(nextMonth)}>
+            <span className="monthName">{nextMonth.format('MMMM')}</span>
+            <span className="arrow">⟩</span>
+          </div>
         </div>
       </section>
     );
   }
 }
+
+Header.propTypes = {
+  'onMonthChange': PropTypes.func.isRequired,
+  'shownMoment': PropTypes.instanceOf(Moment).isRequired
+};
+
 
 export default Header;
